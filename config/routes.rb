f813238194816bb174e477475be75041a0c1ce0b
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-05 20:46:39 +0200

    add categories resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#index'
 
+  resources :categories
+
   get '/about', to: 'homepage#about'
 
   get '/cookies', to: 'homepage#cookies'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
