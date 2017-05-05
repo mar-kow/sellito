db96c0bfa0ee8154037ecf54991d9a84246174ed
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-05 20:59:52 +0200

    generate migration create categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423035342) do
+ActiveRecord::Schema.define(version: 20170505185152) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
