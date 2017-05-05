36c1b7fc250ad20ded930057e5d071ef6e400543
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-05 20:03:32 +0200

    add test index,about,cookies method - response 200

---
+++ spec/controllers/homepage_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe HomepageController do
+  describe '#index' do
+    it 'returns 200' do
+      get :index
+      expect(response.status).to be 200
+    end
+  end
+  describe '#about' do
+    it 'returns 200' do
+      get :index
+      expect(response.status).to be 200
+    end
+  end
+  describe '#cookies' do
+    it 'returns 200' do
+      get :index
+      expect(response.status).to be 200
+    end
+  end
+end
+
