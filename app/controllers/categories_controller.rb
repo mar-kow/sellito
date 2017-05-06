3a0153e77224023c69af23f087601f4bc36ad25d
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-06 18:00:19 +0200

    edit categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,6 +22,23 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update_attributes(category_params)
+    redirect_to @category
+  end
+
+  def destroy
+   @category = Category.find(params[:id])
+   @category.destroy
+   flash[:notice] = "Category #{category.name} deleted"
+   redirect_to categories_path
+  end
+
   private
 
   def category_params
