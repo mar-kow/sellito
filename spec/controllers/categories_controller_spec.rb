e1eaeae03632588a67bd8ede8926c8af49f5d9e6
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-06 16:08:48 +0200

    edit tests in categories controller spec

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,17 +1,53 @@
+#Ładujemy rails helper w każdym spec
 require 'rails_helper'
 
+#Określamy co testujemy - klase CategoriesController, w bloku do - end
 describe CategoriesController do
+  #testujemy metode index, używamy # bo to metoda instancji (. dla klasowych)
   describe '#index' do
-    it 'returns 200' do
-      get :index
-      expect(response.status).to be 200
+    it 'returns 200' do   #wykonujmey konkretny egzemplarz, każdy it to . w testach
+      get :index #wchodzimy na akcje index
+      expect(response.status).to be 200 #spodziewamy się, że status odpowiedzi będzie wynosił 200
     end
   end
 
+ #testujemy metode insancji new -jw
   describe '#new' do
     it 'returns 200' do
       get :new
       expect(response.status).to be 200
+    end
+  end
+
+  describe '#create' do
+    context 'when params are valid' do
+      let(:params) { { category: { name: 'name' } } }
+
+      subject { post :create, params: params }
+
+      it 'returns 302' do
+        subject
+        expect(response.status).to be 302
+      end
+
+      it 'creates a category' do
+        expect { subject }.to change { Category.count }.by(1)
+      end
+    end
+
+    context 'when params are NOT valid' do
+      let(:params) { { category: { name: '' } } }
+
+      subject { post :create, params: params}
+
+      it 'returns 302' do
+        subject
+        expect(response.status).to be 302
+      end
+
+      it 'does NOT create category when name is NOT valid' do
+        expect { subject }.not_to change { Category.count }
+      end
     end
   end
 
@@ -23,5 +59,4 @@ describe CategoriesController do
       expect(response.status).to be 200
     end
   end
-
 end
