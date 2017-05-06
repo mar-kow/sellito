111d5b07cd7abeda7fd104f77f90082541d75d87
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-06 12:25:01 +0200

    add private category_params method, flash notice, redirect back

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,17 @@
 class CategoriesController < ApplicationController
-
   def new
+    @category = Category.new
   end
 
+  def create
+    Category.create(category_params)
+    flash[:notice] = 'Category created!'
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
