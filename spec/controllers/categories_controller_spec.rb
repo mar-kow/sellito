6a4cd3db1b2d8c8c1d90c92e66de66199c14b19b
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-06 14:39:34 +0200

    create test for index and show method

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 describe CategoriesController do
+  describe '#index' do
+    it 'returns 200' do
+      get :index
+      expect(response.status).to be 200
+    end
+  end
+
   describe '#new' do
     it 'returns 200' do
       get :new
       expect(response.status).to be 200
     end
   end
+
+  describe '#show' do
+    let(:category) { Category.create(name: 'test') }
+
+    it 'returns 200' do
+      get :show, params: { id: category.id }
+      expect(response.status).to be 200
+    end
+  end
+
 end
