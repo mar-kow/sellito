4535585f9b1c5cfc8aabcad697b3187f95878b20
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-13 12:55:01 +0200

    validate expiration_date_cannot_be_in_the_past

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   # TODO: handle uploading photos
   belongs_to :user
   validates :title, :expiration_date, presence: true
+
+  validate :expiration_date_cannot_be_in_the_past
+
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date < Date.today
+    errors.add(:expiration_date, "Can't be in the past")
+    end
+  end
+
 end
