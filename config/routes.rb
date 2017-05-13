98fe24dc19c4eb73e71961195d7c418082fc9ef4
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-13 12:29:00 +0200

    create path for my posts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
 
   get '/cookies', to: 'homepage#cookies'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/my_posts', to: 'posts#user_posts'
 end
