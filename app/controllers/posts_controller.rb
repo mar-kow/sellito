40d5e474f8c579adb6d584d956edddcb4015fb4a
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-13 09:58:15 +0200

    update edit, show, update, destroy methods in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-
+  before_action :fetch_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = current_user.posts
+    @posts = current_user.posts.reverse
   end
 
   def new
@@ -20,11 +20,26 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def show;  end
+
+  def edit;  end
+
+  def update
+    @post.update_attributes(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+   @post.destroy
+   flash[:notice] = "Post #{@post.title} deleted"
+   redirect_to posts_path
   end
 
   private
+
+  def fetch_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
      params.require(:post).permit(:title, :description, :expiration_date, :user_id)
