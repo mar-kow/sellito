af6310bf78209d1b69f3e2bc50525c36e6666c2d
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-14 11:41:20 +0200

    add # todo to controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  # TODO: authorization with Pundit gem
+
   before_action :fetch_category, only: %i(show edit update destroy)
   def index
     @categories = Category.all
