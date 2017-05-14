8f721bf43fbb82ce97ad7ee3f98071ad58f6c13e
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-14 14:12:36 +0200

    update seeds by adding categories, user and admin post

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email:'admin2@sellito.pl', password:'password', is_admin?: true)
-User.create(email: 'test@test.pl', password: 'password', is_admin?: false)
+admin = User.create(email: 'admin@sellito.pl', password: 'password', is_admin?: true)
+user = User.create(email: 'klient@sellito.pl', password: 'password')
 
 categories = %w(home fashion garden beauty kids edu food moto it health hobby sport pets music sex other)
 
-categories.each { |category| Catgory.create(name: category) }
+categories.each { |category| Category.create(name: category) }
+
+admin_post_1 = admin.posts.create(title: "Admin Post 1", expiration_date: Time.now + 2.weeks)
+admin_post_2 = admin.posts.create(title: "Admin Post 2", expiration_date: Time.now + 3.weeks)
+admin_post_3 = admin.posts.create(title: "Admin Post 3", expiration_date: Time.now + 4.weeks)
+
+user_post_1 = user.posts.create(title: "User Post 1", expiration_date: Time.now + 2.weeks)
+user_post_2 = user.posts.create(title: "User Post 2", expiration_date: Time.now + 3.weeks)
+user_post_3 = user.posts.create(title: "User Post 3", expiration_date: Time.now + 4.weeks)
+
+admin_post_1.categories << Category.first
+admin_post_1.categories << Category.last
+admin_post_2.categories << Category.first
+admin_post_2.categories << Category.last
+admin_post_2.categories << Category.find_by(name: 'edu')
+admin_post_3.categories << Category.first
+admin_post_3.categories << Category.last
+admin_post_3.categories << Category.find_by(name: 'edu')
+
+user_post_1.categories << Category.find_by(name: 'edu')
+user_post_1.categories << Category.first
+user_post_2.categories << Category.first
+user_post_2.categories << Category.first
+user_post_2.categories << Category.find_by(name: 'kids')
+user_post_3.categories << Category.first
+user_post_3.categories << Category.first
+user_post_3.categories << Category.first
