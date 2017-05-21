7ee52134afcb324677e5f76a77822b158559824e
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-21 14:00:37 +0200

    limit visible posts on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 class HomepageController < ApplicationController
   def index
     @categories = Category.all
-    @posts = Post.all.includes(:categories)
+    @posts = Post.all.includes(:categories).order('created_at DESC').limit(12)
   end
 
   def about; end
